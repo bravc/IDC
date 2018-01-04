bcd7cbbadfc5d487470d11cfa7ba9c8b187bbbdb
Cameron <bravermancj@gmail.com>
2018-01-03 21:03:11 -0500

    added login page

---
+++ README.md
@@ -0,0 +1,28 @@
+# Basic User Registration Website
+
+## Technologies
+* Node.js
+* Express
+* Express Messages, Session, Connect Flash & Validation
+* MongoDB & Mongoose
+* Pug Templating
+* Passport.js Authentication
+* BCrypt Hashing
+
+### Version
+1.0.0
+
+## Usage
+
+
+### Installation
+
+Install the dependencies
+
+```sh
+$ npm install
+```
+Run app
+
+```sh
+$ npm start
--- app.js
+++ app.js
@@ -11,11 +11,52 @@ const app = express();
 
 
 
-
 // Load View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+
+
+// Body Parser Middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+// parse application/json
+app.use(bodyParser.json());
+
+// Set Public Folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Express Session Middleware
+app.use(session({
+  secret: 'keyboard cat',
+  resave: true,
+  saveUninitialized: true
+}));
+
+// Express Messages Middleware
+app.use(require('connect-flash')());
+app.use(function (req, res, next) {
+  res.locals.messages = require('express-messages')(req, res);
+  next();
+});
+
+// Express Validator Middleware
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
+
+    while(namespace.length) {
+      formParam += '[' + namespace.shift() + ']';
+    }
+    return {
+      param : formParam,
+      msg   : msg,
+      value : value
+    };
+  }
+}));
 
 
 
@@ -27,3 +68,11 @@ app.listen(3000, function(){
 app.get('/', function(req, res){
 	res.render('layout');
 });
+
+
+//Allow routing though routes folder
+let users = require('./routes/users');
+app.use('/users', users);
+
+
+module.exports = app;
