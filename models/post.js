df1d17ea717a4f8c7d3812a0e469aca5c822ccf6
Cameron <bravermancj@gmail.com>
2018-01-08 22:31:23 -0500

    more cosmetics

--- models/post.js
+++ models/post.js
@@ -5,7 +5,9 @@ const Schema = mongoose.Schema;
 const PostSchema = Schema({
   author: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
   content: {type: String, required: true},
-  date: {type: String, required:true}
+  date: {type: String, required:true},
+  likes: {type: Number, required: false},
+  dislikes: {type: Number, required: false}
 });
 
 const Post = module.exports = mongoose.model('Post', PostSchema);
