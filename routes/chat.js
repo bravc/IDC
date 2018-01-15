00fe76f7a7cf90fde89df881964ad9e882e8b2c4
Cameron <bravermancj@gmail.com>
2018-01-15 15:01:04 -0500

    Added chat button and new part of panel

--- routes/chat.js
+++ routes/chat.js
@@ -3,13 +3,29 @@ const router = express.Router();
 const io = require('../app').io;
 
 
-router.get('/', function(req, res){
+router.get('/', ensureAuthenticated, function(req, res){
     res.render('chat');
 });
 
-
+//Setup 
 io.on('connection', function(socket){
     console.log('User connected ' + socket.id);
+
+
+    socket.on('chat', function(data){
+        console.log(data);
+    });
 });
+
+
+// Access Control
+function ensureAuthenticated(req, res, next){
+    if(req.isAuthenticated()){
+      return next();
+    } else {
+      req.flash('danger', 'Please login');
+      res.redirect('/');
+    }
+  }
 
 module.exports = router;
