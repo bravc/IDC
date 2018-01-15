46898bd4e3d4b8a5fec4a4958284ba68c1dcc577
Cameron <bravermancj@gmail.com>
2018-01-15 16:07:39 -0500

    added comments and click to send

--- public/js/pssst-chat.js
+++ public/js/pssst-chat.js
@@ -12,10 +12,12 @@ $(document).ready(function(){
     const userName = message.attr('user');
 
 
+    //Show users typing
     message.on('keypress', function(){
         socket.emit('typing', userName);
     });
 
+    //Click function
     btn.on('click', function(){
         if(message.val() != ''){
             socket.emit('chat', {
@@ -26,11 +28,20 @@ $(document).ready(function(){
         }
     });
 
+    //Also allow enter key
+    message.keyup(function(e){
+        if(e.keyCode === 13){
+            btn.click();
+        }
+    });
+
+
     //Show connection
     socket.on('connection', function(){
         console.log('Connected...');
     });
 
+    //When chat is recieved, update the dom
     socket.on('chat', function(data){
         console.log(data);
         feedback.html('');
@@ -39,6 +50,9 @@ $(document).ready(function(){
 
     socket.on('typing', function(data){
         feedback.html('<p><em>' + data + ' is typing...</em></p>');
+        setTimeout(function(){
+            feedback.html('');
+        }, 3000);
     });
 });
 
